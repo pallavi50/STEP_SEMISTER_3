6d589805e7ffde12a50517c85b68850141e87a7c
pallavi50 <pallavi50@gmail.com>
2026-09-20 16:22:21 +0530

    add feature_session2

---
+++ scr/feature_session_2/class_problem/RotateArray.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class RotateArray {
+    static int[] rotateArray(int[] nums, int k) {
+        if (nums.length == 0) {
+            return nums;
+        }
+
+        k = k % nums.length;
+        int[] newArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            newArray[(i + k) % nums.length] = nums[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        System.out.println("Rotated Array: " +
+                Arrays.toString(rotateArray(nums, k)));
+    }
+}
